b8748bfce2d4b63ab90c29df9646287b10001490
Shubham Dubey <shubhamdubey.dubey4@gmail.com>
2024-03-01 01:06:53 +0530

    for obj 2

--- basic_02/03_objects.js
+++ basic_02/03_objects.js
@@ -1,1 +1,34 @@
-//Sing
+//Singleton and Literals these are the ways to define objects
+
+//Object Literals
+
+const mySym = Symbol("key1")
+
+const JsUser = {
+    name: "Shubahm Dubey",
+    "just name":"Shubham",
+    [mySym]:"hii",//proper syntax to use symbol as a key,without normal brackets it will be a normal key
+    age :  54,
+    email : "shubhamdubey.dubey4@gmail.com",
+    isLoggedIn : true,
+    lastLoginDays : ["Monday","Saturday"]
+}
+console.log('JsUser: ', JsUser["just name"]);//this cannon be used with . notation
+console.log('JsUser: ', JsUser['email']);//cannot be used without quotes
+console.log('JsUser: ', JsUser[mySym]);
+
+
+
+//Change values
+
+JsUser.email = "newShubham@gmail.com"
+
+
+JsUser.greetings = function(){
+    console.log("Hello js user");
+}
+
+JsUser.greetingsTwo = function(){
+    console.log(`Hello js user ${this["just name"]}` );
+}
+console.log('JsUser: ', JsUser.greetingsTwo());
---
+++ basic_02/04_objects.js
@@ -0,0 +1,8 @@
+//Singleton Obj
+
+const instaUser = new Object()//singleton
+
+// const instaUser = {}
+
+console.log(instaUser);
+
